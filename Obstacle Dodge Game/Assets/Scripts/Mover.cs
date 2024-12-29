c9e16bf34f0c9760b4c84fad300a0d006ff50756
Sam Funk <borley1@gmail.com>
2024-12-29 13:06:18 -0500

    added basic ground movement and speed

--- Obstacle Dodge Game/Assets/Scripts/Mover.cs
+++ Obstacle Dodge Game/Assets/Scripts/Mover.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+   
+   [SerializeField]float moveSpeed = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0.0f;
+        float zValue = Input.GetAxis("Vertical")* Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,yValue,zValue);
     }
 }
