11dadad723553c10aacf66cb5fdc39fafe71f9fb
Sam Funk <borley1@gmail.com>
2025-01-01 15:54:12 -0500

    refactored Mover and added basic ObjectHit scripts

--- Obstacle Dodge Game/Assets/Scripts/Mover.cs
+++ Obstacle Dodge Game/Assets/Scripts/Mover.cs
@@ -7,11 +7,24 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("To move, use the WASD or arrow keys.");
+        Debug.Log("The object of the game is to get from the start to finish without hitting obstacles.");
+
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0.0f;
